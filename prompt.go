ffa62dbabea658ca1c985c2e8ec58fdb14d4dde2
Louis Buchbinder <git@louisbuchbinder.com>
2023-04-06 17:14:43 -0700

    added vim prompt

--- prompt.go
+++ prompt.go
@@ -11,12 +11,17 @@ var BazelPrompt string
 //go:embed system/prompt/golang.txt
 var GolangPrompt string
 
+//go:embed system/prompt/vim.txt
+var VimPrompt string
+
 func LookupSystemPrompt(key string) string {
 	switch strings.ToLower(key) {
 	case "bazel", "bzl":
 		return BazelPrompt
 	case "go", "golang":
 		return GolangPrompt
+	case "vim", "vimscript", "vimrc":
+		return VimPrompt
 	default:
 		return ""
 	}
